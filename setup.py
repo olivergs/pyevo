e60dcee9554f34e3a04fc5b10805002a1d307810
Oliver <oliver@alpuca.com>
2014-04-15 10:54:26 +0100

    Changed packaging files to be able to install with pip

--- setup.py
+++ setup.py
@@ -9,14 +9,17 @@ PyEVO Python utility library setup script
 .. moduleauthor:: (C) 2013 Oliver Gutiérrez
 """
 # Python imports
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='pyevo',
+    packages =['pyevo'],
     version='0.1',
     description='PyEVO Python utility library',
     author="Oliver Gutiérrez",
     author_email="ogutsua@gmail.com",
-    packages =['pyevo'],
-    platforms=['any'],
+    url = 'https://github.com/R3v1L/pyevo',
+    # download_url = '',
+    keywords = ['pyevo', 'utility', ],
+    classifiers = [],
 )
